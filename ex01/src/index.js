d7dc3cc719cf3ffa004fa36779a8dff8170a884d
Almir2021 <bosnjicalmir@gmail.com>
2021-09-23 23:23:52 +0200

    Vjezba 1

---
+++ ex01/src/index.js
@@ -0,0 +1,20 @@
+// Create an array here
+var myArr = [2,4,0,8,10]
+
+// End of creating an array
+
+  function myArrayFunction(arr) {
+
+    var myItems = [...arr];
+     
+    // Only change code below this line
+
+      myArr = myItems
+
+    return myItems;
+
+
+// Only change code above this line
+    
+
+  } console.log (myArrayFunction([2] = 6));
