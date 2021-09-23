00a643e9c5a536b585a9b6b2eb18f60e528b8d71
Almir2021 <bosnjicalmir@gmail.com>
2021-09-24 01:23:58 +0200

    vjezba 5

---
+++ ex05/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+  var  myNestedArray = [["Toblerone", 5] ["Milka",3]]
+  
+function myNestedFunction (arr) {
+    function myNestedArray () {
+        return myNestedArray;
+    }
+       
+        
+  }
+
+
+
+// Only change code above this line
+console.log (myNestedFunction([arr] ))
+module.exports = myNestedFunction;
