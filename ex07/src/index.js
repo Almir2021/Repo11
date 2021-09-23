1a5684cb7b2a95f417e7d93afaa6b9d7b8ed0c05
Almir2021 <bosnjicalmir@gmail.com>
2021-09-24 00:34:42 +0200

    Vjezba 7

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -21,3 +21,4 @@ function largestNumFromArr (arr) {
 
 // Only change code above this line
  console.log( largestNumFromArr([[13,27,18,26], [4,5,1, 3] , [32,35,37,39] , [1000,1001,857,1] ]))
+ module.exports = largestNumFromArr;
